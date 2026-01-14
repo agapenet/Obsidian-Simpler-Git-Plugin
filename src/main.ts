0bfbf5e951373836e096d7909b8a5ebe877c5514
agapenet <lossnotfunny@gmail.com>
2026-01-13 20:37:15 -0600

    Seamless Git Commands

--- src/main.ts
+++ src/main.ts
@@ -1,99 +1,41 @@
-import {App, Editor, MarkdownView, Modal, Notice, Plugin} from 'obsidian';
-import {DEFAULT_SETTINGS, MyPluginSettings, SampleSettingTab} from "./settings";
+import { Plugin } from "obsidian";
 
-// Remember to rename these classes and interfaces!
+const tag: string = "seamless-git:"
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+export default class SeamlessGit extends Plugin{
+	onload(): void {
+		console.log("Seamless Git Loading..")
 
-	async onload() {
-		await this.loadSettings();
+		// STAGE 0: PULL CHANGES
+		console.log("Pulling Changes")
+		this.runThisCommand("obsidian-git:pull")
 
-		// This creates an icon in the left ribbon.
-		this.addRibbonIcon('dice', 'Sample', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status bar text');
-
-		// This adds a simple command that can be triggered anywhere
 		this.addCommand({
-			id: 'open-modal-simple',
-			name: 'Open modal (simple)',
+			id: tag+"save-commit-push",
+			name: 'Save file, Commit Changes, then Push',
 			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'replace-selected',
-			name: 'Replace selected content',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				editor.replaceSelection('Sample editor command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-modal-complex',
-			name: 'Open modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
 
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-				return false;
-			}
-		});
+				// STAGE 1: SAVE FILE
+				this.runThisCommand("editor:save-file")
 
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			new Notice("Click");
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
-
+				// STAGE 2: COMMIT CHANGES
+				// STAGE 3: PUSH CHANGES TO REMOTE
+				this.runThisCommand("obsidian-git:push")
+      		},
+    	});
 	}
 
-	onunload() {
-	}
+	private runThisCommand(inputId: string): void {
+		const commandId = inputId;
+		const commands = (this.app as any).commands;
+		const commandExist = commands.listCommands().some((cmd: any) => cmd.id === commandId);
 
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData() as Partial<MyPluginSettings>);
-	}
+		if(!commandExist){
+			console.log(tag, "could not find command \"", inputId, "\"")
+			return;
+		}
 
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
-}
-
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
-
-	onOpen() {
-		let {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
+		console.log(tag, "attempting to execute command \"", commandId, "\"")
+		commands.executeCommandById(commandId);
 	}
 }
